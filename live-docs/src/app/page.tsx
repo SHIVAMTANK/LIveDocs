b2cd8d5274d84620ddb8f9683f9b2a758d70cb30
SHIVAMTANK <124774985+SHIVAMTANK@users.noreply.github.com>
2025-04-08 15:56:20 +0530

    Update page.tsx

--- live-docs/src/app/page.tsx
+++ live-docs/src/app/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return(
     <div>
-      <p className="flex min-h-screen items-center justify-center">
+      <p className="flex  min-h-screen items-center justify-center">
         <Button variant="destructive">Click me</Button>
       </p>
     </div>
   )
-}                 
+}         
