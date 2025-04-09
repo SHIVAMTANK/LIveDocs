ac408b056d5c55cfd6200979933d3eec30b572d1
SHIVAMTANK <shivamtank8059@gmail.com>
2025-04-09 19:05:48 +0530

    adding navbar

--- live-docs/src/app/document/[documentid]/page.tsx
+++ live-docs/src/app/document/[documentid]/page.tsx
@@ -1,18 +1,24 @@
 import { Editor } from "./editor";
+import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 interface DocumentPageProps {
-    params: Promise<{documentid: string}>
+  params: Promise<{ documentid: string }>;
 }
 
-const DocumentPage = async ({params}: DocumentPageProps) => {
-    const {documentid} = await params;
+const DocumentPage = async ({ params }: DocumentPageProps) => {
+  const { documentid } = await params;
 
-    return (
-       <div className="min-h-screen bg-[#FAFBFD]">
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden h-[112px]">
+        <Navbar />
         <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
         <Editor />
-       </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default DocumentPage;
