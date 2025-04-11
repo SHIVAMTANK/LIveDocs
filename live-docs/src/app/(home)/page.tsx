6a89b100f2d5b82120706105395c7d44ed9bec23
SHIVAMTANK <shivamtank8059@gmail.com>
2025-04-11 14:27:10 +0530

    adding navbar and search option

--- live-docs/src/app/(home)/page.tsx
+++ live-docs/src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "./navbar";
 export default function Home() {
   return(
     <div className="flex flex-col  min-h-screen">
-      <div className="fixed top-0 left-0 right-0 z-10 h-15 bg-white">
+      <div className="fixed top-0 left-0 right-0 z-10 h-14 bg-white p-4">
         <Navbar />
       </div>
       <div className="mt-16">
@@ -17,3 +17,4 @@ export default function Home() {
     </div>
   )
 }         
+
